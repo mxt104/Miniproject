f758e3701c468b50f27bec5b0b5f90ec94835ead
mxt104 <miteshrane59@gmail.com>
2026-03-15 14:23:35 +0530

    dashboard incomplete

--- frontend/src/pages/AdminDashboard.js
+++ frontend/src/pages/AdminDashboard.js
@@ -131,6 +131,26 @@ const [showCommDropdown, setShowCommDropdown] = useState(false);
 
       <main className="max-w-7xl mx-auto px-6 py-10">
 
+        <div className="flex gap-4 mb-6">
+
+  <button
+    onClick={() => navigate("/admin/add-scholarship")}
+    className="bg-blue-600 text-white px-5 py-2 rounded-lg hover:bg-blue-700 flex items-center gap-2"
+  >
+    <PlusCircle size={18}/>
+    Add Scholarship
+  </button>
+
+  <button
+    onClick={() => navigate("/admin/add-benefit")}
+    className="bg-indigo-600 text-white px-5 py-2 rounded-lg hover:bg-indigo-700 flex items-center gap-2"
+  >
+    <PlusCircle size={18}/>
+    Add Benefit
+  </button>
+
+</div>
+
         {/* ADD SCHOLARSHIP FORM */}
         <div className="bg-white rounded-xl shadow-md p-6 mb-10">
           <h2 className="text-lg font-semibold mb-6 flex items-center gap-2">
